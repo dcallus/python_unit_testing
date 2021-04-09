18ac92faba74d954cf3cec335eab5044bde2fd51
Daniel Callus <danielcallus@gmail.com>
2021-04-09 19:24:08 +0100

    check song list

--- src/room.py
+++ src/room.py
@@ -2,3 +2,5 @@ class Room:
     def __init__(self, guest_list, song_list, max_guests=50):
         self.guest_list = guest_list
         self.song_list = song_list
+
+    
--- tests/test_room.py
+++ tests/test_room.py
@@ -8,12 +8,17 @@ class RoomTest(unittest.TestCase):
     def setUp(self):
         
         self.song_1 = Song("Ring of Fire", "Johnny Cash")
+        self.song_2 = Song("Common People", "Pulp")
         self.guest_1 = Guest("Bob", self.song_1)
+        self.guest_2 = Guest("Rob", self.song_2)
 
         self.room_1_guests = [self.guest_1]
-        self.song_list = [self.song_1]
+        self.song_list = [self.song_1, self.song_2]
 
         self.room_1 = Room(self.room_1_guests, self.song_list)
     
     def test_room_has_guest_list(self):
         self.assertEqual(self.room_1.guest_list, self.room_1_guests)
+
+    def test_room_has_song_list(self):
+        self.assertEqual(self.room_1.song_list, self.song_list)
