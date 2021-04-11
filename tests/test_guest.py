806bb231f81df389699b862876c3a8b716cacc9d
Daniel Callus <danielcallus@gmail.com>
2021-04-11 14:57:02 +0100

    added wallet checks

--- tests/test_guest.py
+++ tests/test_guest.py
@@ -11,9 +11,16 @@ class GuestTest(unittest.TestCase):
         self.song_2 = Song("Song 2", "Blur")
         
         self.guest_1 = Guest("June", self.song_1)
+        self.guest_2 = Guest("April", self.song_1, wallet=50)
 
     def test_guest_name(self):
         self.assertEqual("June", self.guest_1.name)
     
     def test_guest_song(self):
         self.assertEqual(self.song_1, self.guest_1.song)
+
+    def test_guest_default_wallet(self):
+        self.assertEqual(10, self.guest_1.wallet)
+
+    def test_guest_custom_wallet(self):
+        self.assertEqual(50, self.guest_2.wallet)
